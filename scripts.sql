2323c69a802451c38ffa6859a7e1c11f5c36b34c
Victor8069 <victor.zapata8069@gmail.com>
2020-10-27 19:39:54 -0500

    TABLA FICHA

--- scripts.sql
+++ scripts.sql
@@ -9,4 +9,20 @@ CREATE TABLE TblMaterialApoyo(
     MatApoFechaPublicacion	Date NOT NULL,	
     MatApoDescripcion	Varchar(255) NULL,
     TblFases_MaterialApoyo_idFases	Int(5) NOT NULL
+);
+
+CREATE TABLE TblMaterialApoyo_Ficha(
+    IdMatApoFic	Int(5) auto_increment PRIMARY KEY,
+    TblMaterialApoyo_idMaterialApoyo Int(5) NOT NULL,
+    TblFicha_idFicha Int(5) NOT NULL
+);
+
+CREATE TABLE TblFicha(
+    IdFicha	Int(5) auto_increment PRIMARY KEY,
+    FicFechaInicio	Date NOT NULL,	
+    FicFechaFin	Date NOT NULL,	
+    FicNumeroFicha	Varchar(45) NOT NULL,
+    TblTipoJornada_Ficha_idTipJor Int(5) NOT NULL,
+    TblModalidad_Ficha_idMod Int(5) NOT NULL,
+    TblTipoOferta_Ficha_idTipOfe Int(5) NOT NULL
 )
