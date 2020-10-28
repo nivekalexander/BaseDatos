b71cbfd78ba1b54f420447a286c14185c3e86934
Victor8069 <victor.zapata8069@gmail.com>
2020-10-27 19:27:01 -0500

    TABLA MATERIAL APOYO

--- scripts.sql
+++ scripts.sql
@@ -1,4 +1,12 @@
 CREATE TABLE TblFases_MaterialApoyo(
-    IdFases	Int	5,
-    FasNombre Varchar 45
+    IdFases	Int(5) auto_increment PRIMARY KEY,
+    FasNombre Varchar(45) NOT NULL
 );
+
+CREATE TABLE TblMaterialApoyo(
+    IdMaterialApoyo	Int(5) auto_increment PRIMARY KEY,
+    MatApoTitulo	Varchar(45) NOT NULL,
+    MatApoFechaPublicacion	Date NOT NULL,	
+    MatApoDescripcion	Varchar(255) NULL,
+    TblFases_MaterialApoyo_idFases	Int(5) NOT NULL
+)
